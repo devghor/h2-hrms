5869d239d1afbd7460c1e33261be05d6eb7c8739
codeghor <hafij.to@gmail.com>
2025-06-13 20:51:25 +0600

    update query keys

--- client/src/constants/query-keys.ts
+++ client/src/constants/query-keys.ts
@@ -1,9 +1,9 @@
 export const QueryKeys = {
   UAM_USERS: {
-    GET_ALL: ['USERS_GET_ALL'],
-    GET_BY_ID: (id: string) => ['USERS_GET_BY_ID', id],
-    CREATE: ['USERS_CREATE'],
-    UPDATE: ['USERS_UPDATE'],
-    DELETE: ['USERS_DELETE']
+    GET_ALL: ['UAM_USERS_GET_ALL'],
+    GET_BY_ID: (id: string) => ['UAM_USERS_GET_BY_ID', id],
+    CREATE: ['UAM_USERS_CREATE'],
+    UPDATE: ['UAM_USERS_UPDATE'],
+    DELETE: ['UAM_USERS_DELETE']
   }
 };
